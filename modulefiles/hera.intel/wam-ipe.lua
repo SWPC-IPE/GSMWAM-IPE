c27c0c0d2904dbd9fdac8fdfe3734a1de57dfc47
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2024-04-03 19:06:50 +0000

    Changes for spack-stack (on Rocky 8).

--- modulefiles/hera.intel/wam-ipe.lua
+++ modulefiles/hera.intel/wam-ipe.lua
@@ -5,57 +5,50 @@ help([[
 prepend_path("MODULEPATH", "/contrib/sutils/modulefiles")
 load("sutils")
 
-intel_ver=os.getenv("intel_ver") or "18.0.5.274"
-load(pathJoin("intel", intel_ver))
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core")
 
-impi_ver=os.getenv("impi_ver") or "2018.0.4"
-load(pathJoin("impi", impi_ver))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+load(pathJoin("stack-intel", stack_intel_ver))
 
-prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+stack_intel_oneapi_mpi_ver=os.getenv("intel_ver") or "2021.5.1"
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 
-hpc_ver=os.getenv("hpc_ver") or "1.1.0"
-load(pathJoin("hpc", hpc_ver))
+hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
+load(pathJoin("hdf5", hdf5_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
-load(pathJoin("hpc-intel", hpc_intel_ver))
+netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
+load(pathJoin("netcdf-c", netcdf_c_ver))
 
-hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.0.4"
-load(pathJoin("hpc-impi", hpc_impi_ver))
+netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
+load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
 
-hdf5_ver=os.getenv("hdf5_ver") or "1.10.6"
-load(pathJoin("hdf5", hdf5parallel_ver))
-
-netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
-load(pathJoin("netcdf", netcdf_ver))
-
-bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-load(pathJoin("bacio", bacio_ver))
-
-nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
+nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
 load(pathJoin("nemsio", nemsio_ver))
 
-sp_ver=os.getenv("sp_ver") or "2.3.3"
+sp_ver=os.getenv("sp_ver") or "2.5.0"
 load(pathJoin("sp", sp_ver))
 
-w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
-load(pathJoin("w3nco", w3nco_ver))
+parallelio_ver=os.getenv("parallelio_ver") or "2.5.10"
+load(pathJoin("parallelio", parallelio_ver))
 
-esmf_ver=os.getenv("esmf_ver") or "8.4.0b08"
+esmf_ver=os.getenv("esmf_ver") or "8.6.0"
 load(pathJoin("esmf", esmf_ver))
-
-pnetcdf_ver=os.getenv("pnetcdf_ver") or "1.11.2"
-load(pathJoin("pnetcdf", pnetcdf_ver))
-
 
 setenv("CC", "mpiicc")
 setenv("CXX", "mpiicpc")
 setenv("FC", "mpiifort")
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/swpc/Adam.Kubaryk/modulefiles")
+-- comio load occurs out of $HOMEwfs/modulefiles and is implicitly specified
+-- by build.ver as the version of COMIO that is checked out comes from build.ver.
+if os.getenv("HOMEwfs") then
+  prepend_path("MODULEPATH", pathJoin(os.getenv("HOMEwfs"), "modulefiles"))
+else
+  append_path("MODULEPATH", "/scratch1/NCEPDEV/swpc/Adam.Kubaryk/modulefiles")
+end
 comio_ver=os.getenv("comio_ver") or "0.0.10"
 load(pathJoin("comio", comio_ver))
 
-anaconda_ver=os.getenv("anaconda_ver") or "anaconda3-2019.10"
+anaconda_ver=os.getenv("anaconda_ver") or "anaconda3-2024.02"
 load(pathJoin("anaconda", anaconda_ver))
 
 setenv("I_MPI_ADJUST_ALLTOALLV", "0")
